1e34fb58b85ed18e20de81b0589dc52969413ac6
Luca de Alfaro <luca@dealfaro.org>
2018-04-02 16:58:22 -0700

    Now, we can greet people whose names are passed as parameters

--- server.py
+++ server.py
@@ -1,9 +1,13 @@
-from bottle import get, run
+from bottle import get, run, template
 
 @get('/index')
 def index():
     return "Welcome to CMPS 183!"
 
+
+@get('/greet/<name>')
+def greet(name):
+    return template('greet_template', name=name)
 
 run(host='localhost', port=8080)
 
