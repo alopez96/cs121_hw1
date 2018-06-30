c40d4645fcc013d464c24e1338d02b671938a7a7
Arturo <alopez96@ucsc.edu>
2018-06-30 14:01:11 -0700

    final hw1 submission

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from bottle import get, route, run, template, view, static_file
+from bottle import get, route, run, template, view, static_file, debug
+debug(True)
 
 @get('/index')
 def index():
@@ -18,10 +19,19 @@ def greet(name):
 def cats():
     return template('cats_template')
 
+@get('/')
+def news():
+	return template('news_template')
+
 # Let's add some code to serve jpg images from our static images directory.
 @route('/images/<filename:re:.*\.jpg>')
 def serve_image(filename):
     return static_file(filename, root='images', mimetype='image/jpg')
+
+#
+@route('/images/<filename:re:.*\.png>')
+def serve_image(filename):
+    return static_file(filename, root='images', mimetype='image/png')
 
 # Code for serving css stylesheets from /css directory.
 @route('/css/<filename:re:.*.css>')
